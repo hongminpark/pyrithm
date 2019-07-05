4e7ce926e28130611f26def482f0a1208e1b4896
dltnwls0526 <31299595+dltnwls0526@users.noreply.github.com>
2019-07-05 15:34:09 +0900

    동명이인이 여러명일 경우 오류 발생

--- week1_해시1/solmi_해시1_완주하지 못한 선수.py
+++ week1_해시1/solmi_해시1_완주하지 못한 선수.py
@@ -1,3 +1,14 @@
+"""
+동명이인 없으면 else 타고, 동명이인 있으면 counter 로 1개 이상인 애 내뱉음 
+
+But, 
+part, comp = ['a','a','b','b','c','c','c'], ['a','a','b','b','c','c'] 
+solution(part,comp) => ['a', 'b', 'c']
+
+동명이인 여러 개일 경우 오류! 카운터끼리 빼는 과정이 필요할 듯 
+"""
+
+
 from collections import Counter
 
 def solution(participant, completion):
