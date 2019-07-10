d0414cf1e0faa68c79a7ea549dc1ff57dbeceaea
dlckswn <52459782+dlckswn@users.noreply.github.com>
2019-07-10 13:39:49 +0900

    Update dlckswn_sort1.py

--- week2_정렬1/dlckswn_sort1.py
+++ week2_정렬1/dlckswn_sort1.py
@@ -1,7 +1,7 @@
 def solution(array, commands):
     answer = []
     for i in range(len(commands)):
-        arr = array[commands[i][0] - 1:commands[i][1]]
+        arr = array[commands[i][0] - 1:commands[i][1]] #여기서 바로 .sort()하면 값이 다르게 나옴 
         arr.sort()
         answer.append(arr[commands[i][2] - 1])
     return answer
