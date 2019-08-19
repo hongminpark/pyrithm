7ce334d7278c19eb026b5c289c83f0b1f5e71ef9
snslmi <49442190+snslmi@users.noreply.github.com>
2019-08-19 21:10:53 +0900

    Add files via upload

---
+++ week3_탐욕법/solmi_체육복.py
@@ -0,0 +1,12 @@
+def solution(n, lost, reserve):
+    reserved = 0
+    lostN = list(set(lost) - set(reserve))
+    reserveN = list(set(reserve) - set(lost))
+    lostN.sort()
+    for l in lostN:
+        for x in range(l-1, l+2):
+            if x in reserveN:
+                reserveN.remove(x)
+                reserved += 1
+                break
+    return n - len(lostN) + reserved
